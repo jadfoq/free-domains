2a970a0ff807a90e23c24be61ba9e790f898c44b
jadfoq <jadfoq@getgoogleoff.me>
2024-04-21 14:27:26 +0300

    Create nx.1bt.uk.js

---
+++ domains/nx.1bt.uk.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'pubnix', // learn more - 'https://github.com/cwmccabe/pubnixhist'
+  domain: '1bt.uk',
+  subdomain: 'nx',
+  owner: {
+    email: 'jadfoq@disr.it', // matrix - '@jadfoq:nope.chat'
+  },
+  record: {
+    NS: ['ns1.desec.io', 'ns2.desec.org'],
+  },
+  proxy: false,
+})
